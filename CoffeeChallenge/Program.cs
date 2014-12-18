661bd259eba0b7a09f8d37a59386601c9f7b62ba
Tyler Waldron <tylerwal@gmail.com>
2014-12-17 20:09:36 -0500

    First commit of challenge 1 of codingame.com

---
+++ CoffeeChallenge/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+class Player
+{
+	static void Main(string[] args)
+	{
+		FirstChallenge();
+	}
+
+	static void FirstChallenge()
+	{
+		// game loop
+		while (true)
+		{
+			List<KeyValuePair<string, int>> enemies = new List<KeyValuePair<string, int>>();
+
+			int count = int.Parse(Console.ReadLine()); // The number of current enemy ships within range
+			for (int i = 0; i < count; i++)
+			{
+				string[] inputs = Console.ReadLine().Split(' ');
+				string enemy = inputs[0]; // The name of this enemy
+				int dist = int.Parse(inputs[1]); // The distance to your cannon of this enemy
+
+				enemies.Add(new KeyValuePair<string, int>(enemy, dist));
+			}
+			// To debug: Console.Error.WriteLine("Debug messages...");
+
+			Console.Error.WriteLine(enemies.First());
+			Console.WriteLine("Buzz"); // The name of the most threatening enemy (HotDroid is just one example)
+		}
+	}
+}

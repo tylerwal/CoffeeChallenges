941e391dcd37a0b9b7485e8f3ef200ad5eb98ddd
Tyler Waldron <tylerwal@gmail.com>
2014-12-17 21:36:28 -0500

    Challenge - PowerOfThor - complete

--- CoffeeChallenge/Program.cs
+++ CoffeeChallenge/Program.cs
@@ -42,15 +42,81 @@ class Player
 		int xPositionThor = int.Parse(inputs[2]); // Thor's starting X position
 		int yPositionThor = int.Parse(inputs[3]); // Thor's starting Y position
 
+		Queue<string> moves = new Queue<string>();
+
+		int xDistance = xPositionLight - xPositionThor;
+		int xAbsoluteDistance = Math.Abs(xDistance);
+		Console.Error.WriteLine("X Absolute Distance: {0}", xAbsoluteDistance);
+
+		int yDistance = yPositionThor - yPositionLight;
+		int yAbsoluteDistance = Math.Abs(yPositionThor - yPositionLight);
+		Console.Error.WriteLine("Y Absolute Distance: {0}", yAbsoluteDistance);
+
+		int maxDistance = Math.Max(xAbsoluteDistance, yAbsoluteDistance);
+
+		Console.Error.WriteLine("Max Distance: {0}", maxDistance);
+
+		string xDirection = GetEastWest(xDistance);
+		string yDirection = GetNorthSouth(yDistance);
+
+		for (int i = 0; i < maxDistance; i++)
+		{
+			string direction = string.Empty;
+
+			if (i < yAbsoluteDistance)
+			{
+				direction += yDirection;
+			}
+			if (i < xAbsoluteDistance)
+			{
+				direction += xDirection;
+			}
+
+			Console.Error.WriteLine("i: {0} - Move Added: {1}", i, direction);
+
+			moves.Enqueue(direction);
+		}
+
 		// game loop
 		while (true)
 		{
 			int E = int.Parse(Console.ReadLine()); // The level of Thor's remaining energy, representing the number of moves he can still make.
 
-			// Write an action using Console.WriteLine()
-			// To debug: Console.Error.WriteLine("Debug messages...");
+			string move = moves.Dequeue();
 
-			Console.WriteLine("SE"); // A single line providing the move to be made: N NE E SE S SW W or NW
+			Console.WriteLine(move); // A single line providing the move to be made: N NE E SE S SW W or NW
 		}
+	}
+
+	static string GetNorthSouth(int yDistance)
+	{
+		string returnedValue = string.Empty;
+
+		if (yDistance < 0)
+		{
+			returnedValue = "S";
+		}
+		else if (yDistance > 0)
+		{
+			returnedValue = "N";
+		}
+
+		return returnedValue;
+	}
+
+	static string GetEastWest(int xDistance)
+	{
+		string returnedValue = string.Empty;
+
+		if (xDistance > 0)
+		{
+			returnedValue = "E";
+		}
+		else if (xDistance < 0)
+		{
+			returnedValue = "W";
+		}
+
+		return returnedValue;
 	}
 }

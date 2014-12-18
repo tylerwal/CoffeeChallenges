ebdd275de8b294ba273c13a80fe4ad13976e1a4e
Tyler Waldron <tylerwal@gmail.com>
2014-12-17 20:41:38 -0500

    Initial commit of PowerOfThor challenge

--- CoffeeChallenge/Program.cs
+++ CoffeeChallenge/Program.cs
@@ -9,7 +9,6 @@ class Player
 {
 	static void Main(string[] args)
 	{
-		FirstChallenge();
 	}
 
 	static void FirstChallenge()
@@ -32,6 +31,26 @@ class Player
 
 			Console.Error.WriteLine(enemies.OrderBy(i => i.Value).First());
 			Console.WriteLine("Buzz"); // The name of the most threatening enemy (HotDroid is just one example)
+		}
+	}
+
+	static void PowerOfThor()
+	{
+		string[] inputs = Console.ReadLine().Split(' ');
+		int xPositionLight = int.Parse(inputs[0]); // the X position of the light of power
+		int yPositionLight = int.Parse(inputs[1]); // the Y position of the light of power
+		int xPositionThor = int.Parse(inputs[2]); // Thor's starting X position
+		int yPositionThor = int.Parse(inputs[3]); // Thor's starting Y position
+
+		// game loop
+		while (true)
+		{
+			int E = int.Parse(Console.ReadLine()); // The level of Thor's remaining energy, representing the number of moves he can still make.
+
+			// Write an action using Console.WriteLine()
+			// To debug: Console.Error.WriteLine("Debug messages...");
+
+			Console.WriteLine("SE"); // A single line providing the move to be made: N NE E SE S SW W or NW
 		}
 	}
 }

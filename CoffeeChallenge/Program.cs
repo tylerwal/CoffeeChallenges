34ab43fde93e4cb3660dbe547ba99226c48ccb41
Tyler Waldron <tylerwal@gmail.com>
2014-12-19 20:46:09 -0500

    Initial commit of Skynet Chasm challenge

--- CoffeeChallenge/Program.cs
+++ CoffeeChallenge/Program.cs
@@ -164,5 +164,27 @@ class Player
 	}
 
 	#endregion KirksQuest
-	
+
+	#region SkynetChasm
+
+	public static void SkynetChasm()
+	{
+		int R = int.Parse(Console.ReadLine()); // the length of the road before the gap.
+		int G = int.Parse(Console.ReadLine()); // the length of the gap.
+		int L = int.Parse(Console.ReadLine()); // the length of the landing platform.
+
+		// game loop
+		while (true)
+		{
+			int S = int.Parse(Console.ReadLine()); // the motorbike's speed.
+			int X = int.Parse(Console.ReadLine()); // the position on the road of the motorbike.
+
+			// Write an action using Console.WriteLine()
+			// To debug: Console.Error.WriteLine("Debug messages...");
+
+			Console.WriteLine("SPEED"); // A single line containing one of 4 keywords: SPEED, SLOW, JUMP, WAIT.
+		}
+	}
+
+	#endregion SkynetChasm
 }

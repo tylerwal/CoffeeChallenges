2d16c13e26c0bea30c4f3f769b4496103be39f47
Tyler Waldron <tylerwal@gmail.com>
2014-12-21 00:33:17 -0500

    Mars Lander Part 1 : Challenge completed

--- CoffeeChallenge/Program.cs
+++ CoffeeChallenge/Program.cs
@@ -230,13 +230,25 @@ class Player
 	public static void MarsLanderPart1()
 	{
 		string[] inputs;
-		int N = int.Parse(Console.ReadLine()); // the number of points used to draw the surface of Mars.
-		for (int i = 0; i < N; i++)
+
+		// the number of points used to draw the surface of Mars.
+		int numberPointsMarsSurface = int.Parse(Console.ReadLine());
+
+		for (int i = 0; i < numberPointsMarsSurface; i++)
 		{
 			inputs = Console.ReadLine().Split(' ');
-			int LAND_X = int.Parse(inputs[0]); // X coordinate of a surface point. (0 to 6999)
-			int LAND_Y = int.Parse(inputs[1]); // Y coordinate of a surface point. By linking all the points together in a sequential fashion, you form the surface of Mars.
+
+			// X coordinate of a surface point. (0 to 6999)
+			int xSurfacePoint = int.Parse(inputs[0]);
+			Console.Error.WriteLine("X Surface Point: {0}", xSurfacePoint);
+
+			// Y coordinate of a surface point. By linking all the points together in a sequential fashion, 
+			// you form the surface of Mars.
+			int ySurfacePoint = int.Parse(inputs[1]);
+			Console.Error.WriteLine("Y Surface Point: {0}", ySurfacePoint);
 		}
+
+		int thrust = 3;
 
 		// game loop
 		while (true)
@@ -244,16 +256,29 @@ class Player
 			inputs = Console.ReadLine().Split(' ');
 			int X = int.Parse(inputs[0]);
 			int Y = int.Parse(inputs[1]);
-			int HS = int.Parse(inputs[2]); // the horizontal speed (in m/s), can be negative.
-			int VS = int.Parse(inputs[3]); // the vertical speed (in m/s), can be negative.
-			int F = int.Parse(inputs[4]); // the quantity of remaining fuel in liters.
-			int R = int.Parse(inputs[5]); // the rotation angle in degrees (-90 to 90).
-			int P = int.Parse(inputs[6]); // the thrust power (0 to 4).
 
-			// Write an action using Console.WriteLine()
-			// To debug: Console.Error.WriteLine("Debug messages...");
+			int horizontalSpeed = int.Parse(inputs[2]); // the horizontal speed (in m/s), can be negative.
 
-			Console.WriteLine("-20 3"); // R P. R is the desired rotation angle. P is the desired thrust power.
+			int verticalSpeed = int.Parse(inputs[3]); // the vertical speed (in m/s), can be negative.
+
+			int fuelRemaining = int.Parse(inputs[4]); // the quantity of remaining fuel in liters.
+
+			int rotationalAngle = int.Parse(inputs[5]); // the rotation angle in degrees (-90 to 90).
+
+			int thrustPower = int.Parse(inputs[6]); // the thrust power (0 to 4).
+
+			if (verticalSpeed < -20 && thrust <= 3)
+			{
+				Console.Error.WriteLine("Thrust increased");
+				thrust++;
+			}
+			else if (verticalSpeed > -7 && thrust >= 0)
+			{
+				Console.Error.WriteLine("Thrust decreased");
+				thrust--;
+			}
+
+			Console.WriteLine("0 {0}", thrust); // R P. R is the desired rotation angle. P is the desired thrust power.
 		}
 	}
 

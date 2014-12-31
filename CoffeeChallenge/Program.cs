6f88badecf13dca3b5b97c54dc65e26045ba1fe4
Tyler Waldron <tylerwal@gmail.com>
2014-12-30 22:13:05 -0500

    Initial commit of Defibrillators challege

--- CoffeeChallenge/Program.cs
+++ CoffeeChallenge/Program.cs
@@ -511,4 +511,24 @@ class Player
 	}
 
 	#endregion MimeType
+
+	#region Defibrillators
+
+	public static void Defibrillators()
+	{
+		string LON = Console.ReadLine();
+		string LAT = Console.ReadLine();
+		int N = int.Parse(Console.ReadLine());
+		for (int i = 0; i < N; i++)
+		{
+			string DEFIB = Console.ReadLine();
+		}
+
+		// Write an action using Console.WriteLine()
+		// To debug: Console.Error.WriteLine("Debug messages...");
+
+		Console.WriteLine("answer");
+	}
+
+	#endregion Defibrillators
 }

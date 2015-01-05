ac9c30d67e1553b1480e0f2407d529e0fa93f0a9
Tyler Waldron <tylerwal@gmail.com>
2015-01-04 22:36:57 -0500

    Initial commit of Skynet Finale Part 1 challenge

--- CoffeeChallenge/Program.cs
+++ CoffeeChallenge/Program.cs
@@ -669,4 +669,38 @@ class Player
 	}
 
 	#endregion HorseRacing
+
+	#region SkynetFinalPart1
+
+	public static void SkynetFinalPart1()
+	{
+	   string[] inputs;
+	   inputs = Console.ReadLine().Split(' ');
+	   int N = int.Parse(inputs[0]); // the total number of nodes in the level, including the gateways
+	   int L = int.Parse(inputs[1]); // the number of links
+	   int E = int.Parse(inputs[2]); // the number of exit gateways
+	   for (int i = 0; i < L; i++)
+	   {
+		  inputs = Console.ReadLine().Split(' ');
+		  int N1 = int.Parse(inputs[0]); // N1 and N2 defines a link between these nodes
+		  int N2 = int.Parse(inputs[1]);
+	   }
+	   for (int i = 0; i < E; i++)
+	   {
+		  int EI = int.Parse(Console.ReadLine()); // the index of a gateway node
+	   }
+
+	   // game loop
+	   while (true)
+	   {
+		  int SI = int.Parse(Console.ReadLine()); // The index of the node on which the Skynet agent is positioned this turn
+
+		  // Write an action using Console.WriteLine()
+		  // To debug: Console.Error.WriteLine("Debug messages...");
+
+		  Console.WriteLine("0 1"); // Example: 0 1 are the indices of the nodes you wish to sever the link between
+	   }
+    }
+
+	#endregion SkynetFinalPart1
 }
